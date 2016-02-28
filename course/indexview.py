7edd90daad15a1de003a0de8c41992c8990f1604
Sankar Manoj <me@sankar-manoj.com>
2016-02-28 19:47:33 +0530

    Added Side Bar Collapsible Menu with one placeholder chapter

--- course/indexview.py
+++ course/indexview.py
@@ -2,5 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from models import *
 def index(request):
-    firstPage = Page.objects.get()
-    return render(request,"index.html",{'page':firstPage})
+    firstPage = Page.objects.all()[0]
+    chapter=Chapter.objects.all()[0]
+    return render(request,"index.html",{'page':firstPage,'chapter':chapter})
--- course/models.py
+++ course/models.py
@@ -7,13 +7,35 @@ class Page(models.Model):
     title=models.CharField(max_length=50)
 class Content(models.Model):
     body=models.TextField()
+    header = models.TextField(blank=True)
+    footer = models.TextField(blank=True)
+    name=models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 class Topic(models.Model):
     name = models.CharField(max_length=200)
-    contents = models.ManytoManyField(Content)
+    header = models.TextField(blank=True)
+    footer = models.TextField(blank=True)
+    contents =  models.ManyToManyField(Content,blank=True)
+    def __unicode__(self):
+        return self.name
 class Chapter(models.Model):
     name = models.CharField(max_length=200)
+    header = models.TextField(blank=True)
+    footer = models.TextField(blank=True)
     position=models.IntegerField()
-    topics = models.ManytoManyField(Topic)
+    topics = models.ManyToManyField(Topic,blank=True)
+    def __unicode__(self):
+        return self.name
 class PageAdmin(admin.ModelAdmin):
     pass
+class ChapterAdmin(admin.ModelAdmin):
+    pass
+class ContentAdmin(admin.ModelAdmin):
+    pass
+class TopicAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Chapter,ChapterAdmin)
+admin.site.register(Topic,TopicAdmin)
+admin.site.register(Content,ContentAdmin)
 admin.site.register(Page,PageAdmin)
